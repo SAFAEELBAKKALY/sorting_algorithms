4f3788c94e2b444c72416feb9b220318822697c2
Mohammed Amine Mounjid <medamine190@gmail.com>
2023-09-23 19:16:13 +0100

    task 0

--- sort.h
+++ sort.h
@@ -6,8 +6,6 @@
 #include <string.h>
 #include <unistd.h>
 
-void print_array(const int *array, size_t size);
-void print_list(const listint_t *list);
 
 /**
  * struct listint_s - Doubly linked list node
@@ -18,35 +16,13 @@ void print_list(const listint_t *list);
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
-
-#ifndef SORT_H
-#define SORT_H
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
 
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
-
-/**
- * struct listint_s - Doubly linked list node
- *
- * @n: Integer stored in the node
- * @prev: Pointer to the previous element of the list
- * @next: Pointer to the next element of the list
- */
-typedef struct listint_s
-{
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
-} listint_t;
 
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
